c8c677b67a09b6d3806ee6192d7e2d861ce4e2d4
Christian Reinbacher <reinbacher@icg.tugraz.at>
2016-07-05 08:05:44 +0200

    added appglance

---
+++ src/appglance.h
@@ -0,0 +1,17 @@
+//
+//  appglance.h
+//  alarmsplusplus
+//
+//  Created by Christian Reinbacher on 15.06.16.
+//
+//
+
+#ifndef appglance_h
+#define appglance_h
+
+#include <pebble.h>
+#include "drink.h"
+
+void update_app_glance(Drink* drinks);
+
+#endif /* appglance_h */
